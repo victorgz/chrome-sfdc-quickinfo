27b9142589461fac52309c89512a447faf8311cd
Victor Garcia Zarco <victor.garciazarco@salesforce.com>
2019-10-29 14:44:43 +0100

    Change url targets

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(function(){
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {urlMatches: 'https://xxx.lightning.force.com/lightning/r/Lead/'},
+          pageUrl: {urlContains: 'lightning.force.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
