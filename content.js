7a4209510e9f46213953c8801a2954d0de460ed8
Victor Garcia Zarco <victor.garciazarco@salesforce.com>
2019-10-29 14:44:05 +0100

    Refactor code

--- content.js
+++ content.js
@@ -1,39 +1,40 @@
 console.log("Starting content script");
 /* Variables used to store the fields of the Lead */
-var field1 = searchFields("Name");
-var field2 = searchFields("Lead Status");
-var field3 = searchFields("Phone");
-var field4 = searchFields("Email");
+
+var fields = ["Name", "Lead Status", "Phone", "Email"];
 
 
 /* JSON Array with the fields (labels and values) of the Lead */
-var leadData = [
-	{
-    	'label': "Lead Name",
-    	'value': field1
-    },
-    {
-    	'label': "Status",
-    	'value': field2
-    },
-    {
-    	'label': "Phone number",
-    	'value': field3
-    },
-    {
-    	'label': "Email",
-    	'value': field4
-    }
-    ];
+var leadData = initJsonArray(fields);
 
     chrome.storage.local.set({
             'leadData': leadData
     });
 
 
+/* Initializes the JSON Array */
+function initJsonArray(fields){
+    console.log("Going to init array: " + fields);
+    var data = [];
+
+    fields.forEach(function (item, index) {
+        var aux = searchFields(item);
+        var newItem = {
+            'label': item,
+            'value': aux.replace('Click to dial disabled','')
+        };
+        data.push(newItem);
+    });
+    return data;
+}
+
 /* Searches in all the HTML elements by the given field label, returning the content of the value element */
 function searchFields(fieldLabel){
-	var labelElements = document.body.getElementsByClassName("windowViewMode-normal active")[0].getElementsByClassName("test-id__field-label");
+    var mainContainer = document.body.getElementsByClassName("windowViewMode-normal active")[0];
+    if(mainContainer == null)
+        return "";
+
+	var labelElements = mainContainer.getElementsByClassName("test-id__field-label");
 	var searchText = fieldLabel;
 	var found;
 	var fieldValue;
